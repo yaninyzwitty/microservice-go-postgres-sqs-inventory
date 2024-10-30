e68655a1ce30e6f3c84dd24de6e273cb25784a6c
yaninyzwitty <ianmwa143@gmail.com>
2024-10-30 17:16:00 +0300

    add config to main,go

--- main.go
+++ main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"log/slog"
+	"os"
+	"time"
+
+	"github.com/yaninyzwitty/sqs-postgres-microservice-inventory/internal/pkg"
+)
 
 func main() {
-	fmt.Println("Hello there!")
+	file, err := os.Open("config.yaml")
+	if err != nil {
+		slog.Error("failed to open config file", "error", err)
+		os.Exit(1)
+	}
+
+	defer file.Close()
+	if err != nil {
+		slog.Error("failed to open config file", "error", err)
+		os.Exit(1)
+	}
+	_, cancel := context.WithTimeout(context.Background(), 32*time.Second)
+	defer cancel()
+	var cfg pkg.Config
+
+	if err := cfg.LoadConfig(file); err != nil {
+		slog.Error("failed to load config", "error", err)
+		os.Exit(1)
+
+	}
+
 }
