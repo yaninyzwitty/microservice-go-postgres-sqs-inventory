3373a8180788e614aa76bb39acd7e75f766c638f
yaninyzwitty <ianmwa143@gmail.com>
2024-10-30 17:40:20 +0300

    add sqs, db logic in main

--- main.go
+++ main.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log/slog"
 	"os"
 	"time"
 
+	"github.com/yaninyzwitty/sqs-postgres-microservice-inventory/internal/aws"
+	"github.com/yaninyzwitty/sqs-postgres-microservice-inventory/internal/database"
 	"github.com/yaninyzwitty/sqs-postgres-microservice-inventory/internal/pkg"
 )
 
@@ -21,7 +24,7 @@ func main() {
 		slog.Error("failed to open config file", "error", err)
 		os.Exit(1)
 	}
-	_, cancel := context.WithTimeout(context.Background(), 32*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 32*time.Second)
 	defer cancel()
 	var cfg pkg.Config
 
@@ -30,5 +33,32 @@ func main() {
 		os.Exit(1)
 
 	}
+
+	db, err := database.NewDatabaseConnection(ctx, cfg.Database.DATABASE_URL)
+	if err != nil {
+		slog.Error("failed to connect to database", "error", err)
+		os.Exit(1)
+	}
+	// ping the database
+	err = database.PingDatabase(ctx, db)
+	if err != nil {
+		slog.Error("failed to ping database", "error", err)
+		os.Exit(1)
+	}
+	defer db.Close()
+
+	sqsClient, err := aws.LoadSQSClient(ctx, cfg.AWS.Region)
+	if err != nil {
+		slog.Error("failed to load  and create sqs client", "error", err)
+		os.Exit(1)
+	}
+
+	queueURL, err := aws.CreateQueueURL(ctx, "witty-queue", sqsClient)
+	if err != nil {
+		slog.Error("failed to create queue url", "error", err)
+		os.Exit(1)
+	}
+
+	fmt.Println(queueURL)
 
 }
